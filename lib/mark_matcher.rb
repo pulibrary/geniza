7a6de698ee573f31e35d73e8565ba706c01873e6
Cliff Wulfman <cwulfman@princeton.edu>
2021-11-19 09:13:59 -0500

    refining rules

--- lib/mark_matcher.rb
+++ lib/mark_matcher.rb
@@ -10,8 +10,48 @@ class MarkMatcher
     @dest = Pathname(dest)
     @kb = {}
 
-    add_rule(/^(?<lib>ENA|NS|MS)_(?<id>[^_]+)_0*(?<leaf>\d+)_[rv]\.tiff?$/,
-             ->(m) { File.join(dest, m[:lib], m[:id], m[:leaf].rjust(3, "0")) })
+    # ENA 2056.1
+    add_rule(/^(?<lib>ENA|NS)\s+(?<id>\d+)\.0*(?<leaf>\d+)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0"), m[:leaf].rjust(6, "0")) })
+
+    # ENA 1822A.1
+    add_rule(/^(?<lib>ENA|NS)\s+(?<id>\d+[A-Z])\.0*(?<leaf>\d+)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0"), m[:leaf].rjust(6, "0")) })
+    
+    add_rule(/^(?<lib>ENA|NS)\s+(?<id>\d+)uler.*?$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0")) })
+
+    add_rule(/^(?<lib>ENA)\s+(?<id>\d+)\.(?<sub>[A-Z])\.(?<leaf>\d+)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0"), m[:sub], m[:leaf].rjust(6, "0")) })
+
+    add_rule(/^(?<lib>ENA)\s+(?<id>\d+)\.(?<sub>[A-Z])uler.*?$/,
+             ->(m) { File.join(dest, m[:lib], m[:id], m[:sub]) })
+
+
+
+    add_rule(/^(?<lib>ENA)\s+(?<sub>NS)\s+(?<id>I)uler.*$/,
+             ->(m) { File.join(dest, m[:lib], m[:sub], m[:id]) })
+
+    add_rule(/^(?<lib>ENA)\s+(?<sub>NS)\s+(?<id>I)\.0*(?<leaf>\d+\w?)$/,
+             ->(m) { File.join(dest, m[:lib], m[:sub], m[:id], m[:leaf].rjust(6, "0")) })
+
+    add_rule(/^(?<lib>ENA)\s+(?<sub>NS)\s+(?<id>I)\.0*(?<leaf>\d+\w?)$/,
+             ->(m) { File.join(dest, m[:lib], m[:sub], m[:id], m[:leaf].rjust(6, "0")) })
+
+    add_rule(/^(?<lib>ENA)\s+(?<sub>NS)\s+(?<id>\d+)\.0*(?<leaf>\d+)$/,
+             ->(m) { File.join(dest, m[:lib], m[:sub], m[:id].rjust(6, "0"), m[:leaf].rjust(6, "0")) })
+
+
+    add_rule(/^(?<lib>Schechter|KE|Krengel)\.(?<id>\d+\w?)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0")) })
+
+    add_rule(/^(?<lib>Schechter|KE|Krengel)uler\.(?<id>\d+\w?)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0")) })
+
+    # MS.L501.19
+    add_rule(/^(?<lib>MS)\.(?<id>[A-Z]\d+)\.0*(?<leaf>\d+\w?)$/,
+             ->(m) { File.join(dest, m[:lib], m[:id].rjust(6, "0"), m[:leaf].rjust(6, "0")) })
+
 
   end
 
@@ -25,7 +65,7 @@ class MarkMatcher
     result = ''
     kb.each do |pattern, action|
       if m = shelfmark.match(pattern)
-        result = action.call(m)
+        result = Pathname.new action.call(m)
         break
       end
     end
--- spec/mark_matcher_spec.rb
+++ spec/mark_matcher_spec.rb
@@ -10,11 +10,35 @@ RSpec.describe MarkMatcher do
   let(:tests) do
     [
       {  mark: 'ENA 1056.1',
-        target: 'geniza/ENA/1056/001/ENA_1056_001_r.jp2'},
+        target: 'geniza/ENA/001056/000001'},
       { mark: 'ENA NS I.99',
-        target: 'geniza/ENA/NS/I/099/ENA_NS_I_099_v.jp2'},
+        target: 'geniza/ENA/NS/I/000099'},
       { mark: 'ENA NS I.96b',
-        target: 'geniza/ENA/NS/I/096b/ENA_NS_I_096b_v.jp2'},
+        target: 'geniza/ENA/NS/I/00096b'},
+
+      { mark: 'Krengel.22',
+       target: 'geniza/Krengel/000022'},
+
+      { mark: 'ENA NS Iuler.1',
+        target: 'geniza/ENA/NS/I'},
+
+      { mark: 'Schechteruler.1',
+       target: 'geniza/Schechter/000001'},
+
+      { mark: 'KE.1',
+       target: 'geniza/KE/000001'},
+
+      { mark: 'Krengel.78b',
+       target: 'geniza/Krengel/00078b' },
+      { mark: 'ENA 1822A.1',
+       target: 'geniza/ENA/01822A/000001' },
+      { mark: 'MS.L501.19',
+       target: 'geniza/MS/00L501/000019' },
+      { mark: 'MS.L593.4a',
+       target: 'geniza/MS/00L593/00004a' },
+      { mark: 'NS 9.33',
+       target: 'geniza/NS/000009/000033' }
+
 
     ]
   end
@@ -23,7 +47,7 @@ RSpec.describe MarkMatcher do
   describe '#target_path' do
     it 'can generate paths' do
       tests.each do |test|
-        expect(marker.target_path(Pathname(test[:mark]))).to eq(Pathname(test[:target]))
+        expect(marker.target_path(test[:mark])).to eq(Pathname(test[:target]))
       end
     end
   end
